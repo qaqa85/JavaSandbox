c195b0ddacb50732a3a7b4b58efe43beb7d2bcc8
qaqa85 <qaqa.tanski@gmail.com>
2022-02-17 12:13:16 +0100

    Pageable

--- src/main/java/com/webExample/demo/controller/TaskController.java
+++ src/main/java/com/webExample/demo/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.webExample.demo.controller;
 import com.webExample.demo.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +21,11 @@ import org.springframework.web.bind.annotation.GetMapping;
     ResponseEntity<?> readAllTasks () {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<?> readAllTasks(Pageable page) {
+        logger.info("Custom pageable!");
+        return ResponseEntity.ok(repository.findAll(page));
     }
 }
